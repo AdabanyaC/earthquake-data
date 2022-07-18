7729e03822b10727b8e6b1c1ded11b518c705636
Clinton Adabanya <clintonadabanya@gmail.com>
2022-07-18 12:25:49 +0100

    Responsive Design on the List and Map components for Wider screens

--- src/components/SortedEarthqualkeList.jsx
+++ src/components/SortedEarthqualkeList.jsx
@@ -9,7 +9,7 @@ const SortedEartquakeList = ({
 }) => {
   return (
     <div
-      className={`bg-white w-full md:w-[36%] h-[33rem] shadow-xl rounded-md overflow-auto`}
+      className={`bg-white w-full md:w-[36%] h-[33rem] lg:h-[85vh] shadow-xl rounded-md overflow-auto`}
     >
       <div className="p-5 shadow-lg sticky top-0 bg-purple-700 text-white">
         {error ? (
